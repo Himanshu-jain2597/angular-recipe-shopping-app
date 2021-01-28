138390ee23d338c9beb8e37deee64624837b7226
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-28 18:38:10 +0530

    Update recipe-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
-    new Recipe('A test recipe','This is simply a test','https://images.app.goo.gl/5Az1Ko39v8idegV5A')
+    new Recipe('A test recipe','This is simply a test','https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg')
   ];
 
   constructor() { }
