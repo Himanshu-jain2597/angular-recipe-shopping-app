12d3a29677a99447bf6fbccc611ce80be5de2068
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-28 19:13:35 +0530

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingeridents.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
 export class ShoppingListComponent implements OnInit {
+  ingeridents:Ingredient[]=[
+    new Ingredient('apples',5),
+    new Ingredient('mongoes',10)
+  ];
 
   constructor() { }
 
