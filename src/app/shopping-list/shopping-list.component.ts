55d9aeeb7d1986582cff4d10bdad65a781db887f
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-29 14:17:04 +0530

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  oningeridentAdded(ingeridents:Ingredient) {
+    this.ingeridents.push(ingeridents);
+  }
+
 }
