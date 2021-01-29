a6cc6565d2eb18f8d96f3ecc1c952a761ad9637c
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-29 18:51:15 +0530

    emit recipes through serivces

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
 
   recipes:Recipe[] = [
     new Recipe('A test recipe','This is simply a test','https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg'),
