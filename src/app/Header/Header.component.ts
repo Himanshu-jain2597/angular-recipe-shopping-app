ff134cf8b0f6375dfd0fb96bb818844bcfcc6f74
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-29 12:52:06 +0530

    added navigation with event

--- src/app/Header/Header.component.ts
+++ src/app/Header/Header.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import {Component,EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector:'app-header',
   templateUrl:'./Header.component.html'
 })
 export class HeaderComponent {
+
+  @Output()
+  featuredSelected= new EventEmitter<string>();
+
+  onSelect(feature:string) {
+    this.featuredSelected.emit(feature);
+
+  }
 
 }
