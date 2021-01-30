a7df7f3cf836f8168c0058e0864c22c2d18e7f45
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-30 16:28:01 +0530

    add router link

--- src/app/Header/Header.component.ts
+++ src/app/Header/Header.component.ts
@@ -6,12 +6,12 @@ import {Component,EventEmitter, Output} from '@angular/core';
 })
 export class HeaderComponent {
 
-  @Output()
-  featuredSelected= new EventEmitter<string>();
+  // @Output()
+  // featuredSelected= new EventEmitter<string>();
 
-  onSelect(feature:string) {
-    this.featuredSelected.emit(feature);
+  // onSelect(feature:string) {
+  //   this.featuredSelected.emit(feature);
 
-  }
+  // }
 
 }
