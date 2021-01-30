179f6370deb556e2a792aa4d8debeb5bf2a5e7c4
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-30 13:04:39 +0530

    add ingredient in recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from "../shared/ingeridents.model";
+
 export class Recipe {
  public name: string;
  public description: string;
  public imgPath:string;
+ public ingreidents:Ingredient[];
 
- constructor(name:string,description:string,imgPath:string) {
+ constructor(name:string,description:string,imgPath:string,ingreidents:Ingredient[]) {
    this.name = name;
    this.description = description;
    this.imgPath = imgPath;
+   this.ingreidents=ingreidents;
  }
 
 }
