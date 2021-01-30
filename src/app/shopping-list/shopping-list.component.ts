33c2fdba97f36477d1ce04687288a7b2a651bc04
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-30 12:16:55 +0530

    emit shopping list with service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingeridents.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
 export class ShoppingListComponent implements OnInit {
-  ingeridents:Ingredient[]=[
-    new Ingredient('apples',5),
-    new Ingredient('mongoes',10)
-  ];
+    ingeridents:Ingredient[];
+  // ingeridents:Ingredient[]=[
+  //   new Ingredient('apples',5),
+  //   new Ingredient('mongoes',10)
+  // ];
 
-  constructor() { }
+  constructor(private slService:ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingeridents= this.slService.getIngeridents();
+    this.slService.ingeridentsChanged.subscribe((ingeridents:Ingredient[])=> {
+      this.ingeridents=ingeridents;
+    })
   }
 
-  oningeridentAdded(ingeridents:Ingredient) {
-    this.ingeridents.push(ingeridents);
-  }
+  // oningeridentAdded(ingeridents:Ingredient) {
+  //   this.ingeridents.push(ingeridents);
+  // }
 
 }
