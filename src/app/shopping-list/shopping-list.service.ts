ea3230e3c28c5e090434fe2c83b7bcac46b950be
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-30 12:17:31 +0530

    emit shopping list with service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from '@angular/core';
+import {Ingredient} from '../shared/ingeridents.model';
+
 export class ShoppingListService {
+  ingeridentsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingeridents:Ingredient[]=[
+    new Ingredient('apples',5),
+    new Ingredient('mongoes',10)
+  ];
+
+  getIngeridents() {
+    return this.ingeridents.slice();
+  }
+
+  addIngerident(ingerident:Ingredient) {
+    this.ingeridents.push(ingerident);
+    this.ingeridentsChanged.emit(this.ingeridents.slice())
+  }
 
 }
