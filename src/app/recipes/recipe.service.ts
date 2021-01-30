87d3ba987b6d9b25ebd208c8e0ea78153fb62977
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-30 13:05:12 +0530

    add ingredient in recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingeridents.model';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,21 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes:Recipe[] = [
-    new Recipe('A test recipe','This is simply a test','https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg'),
-    new Recipe('A test recipe','This is simply a test','https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg')
+    new Recipe(
+      'Burger',
+      'to make a burger',
+      'https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg',
+      [
+        new Ingredient('meat',10),
+        new Ingredient('sabzi',20)
+      ]),
+    new Recipe('Pizza',
+    'to make pizza',
+    'https://images.pexels.com/photos/4737039/pexels-photo-4737039.jpeg?cs=srgb&dl=pexels-adriano-brodbeck-4737039.jpg&fm=jpg',
+    [
+      new Ingredient('alloo',10),
+      new Ingredient('bangan',20)
+    ])
   ];
 
   getRecipes() {
