5bb149c23dbf443a81114d0d81430d2b13dfece3
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-01-31 12:03:52 +0530

    make recipe clickable

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,12 +9,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input()
+  // @Input()
   recipe:Recipe;
-
-  constructor() { }
+  id:number;
+  constructor(private recipeService:RecipeService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+    .subscribe((params:Params)=> {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    })
+
   }
 
 }
